8f1b878a5d83e19696696dc3f8210523c8c2a839
kaoru-kitajima <kaoru.kitta@gmail.com>
2018-10-14 18:19:07 +0900

    second commit

---
+++ sample2.py
@@ -0,0 +1,12 @@
+#%%
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+
+range = 50
+x = np.arange(range)
+y = np.random.randn(range)
+plt.plot(x, y)
+cos_x = np.cos(x)
+sin_x = np.sin(x)
+plt.plot(cos_x, sin_x)
